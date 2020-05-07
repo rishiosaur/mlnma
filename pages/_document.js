d3e6a260b6aeea61c73b3205943577a8eec458b2
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-06 21:50:45 -0400

    add in meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,27 @@ export default class MyDocument extends Document {
             }
           </Media>
           <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.2/highlight.min.js"></script>
+          <meta name="title" content="MLNMA"/>
+<meta name="description" content="A notebook, blog, and little course about Machine Learning, without the abstractness."/>
+<meta name="keywords" content="Machine Learning, machine learning, artificial intelligence, mathematics, javascript, rust, python, numpy, tensorflow, keras, nextjs, web development"/>
+<meta name="robots" content="index, follow"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="language" content="English"/>
+<meta name="revisit-after" content="1 days"/>
+<meta name="author" content="Rishi Kothari"/>
+<meta name="title" content="MLNMA"/>
+<meta name="description" content="A course, notebook, and blog about Machine Learning, Web Development, and Math without the abstractness.."/>
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://mlnma.now.sh/"/>
+<meta property="og:title" content="MLNMA"/>
+<meta property="og:description" content="A course, notebook, and blog about Machine Learning, Web Development, and Math without the abstractness.."/>
+<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+<meta property="twitter:card" content="summary_large_image"/>
+<meta property="twitter:url" content="https://mlnma.now.sh/"/>
+<meta property="twitter:title" content="MLNMA"/>
+<meta property="twitter:description" content="A course, notebook, and blog about Machine Learning, Web Development, and Math without the abstractness.."/>
+<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>
+
         </Head>
         <body>
           <Main />
